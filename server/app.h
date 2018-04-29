33119781c4f67bba73d56bfc498c0e05ce9f63e2
Franco Risma <franco.emilio.risma@fisglobal.com>
2018-04-28 23:07:12 -0300

    Server header documentation

--- server/app.h
+++ server/app.h
@@ -24,12 +24,19 @@
 		int csd;
 	}threadData;
 
+	// Esta es la funcion inicial que crea el contexto necesario para el funcionamiento, crea la cola de mensajes,
+	// obtiene la configuracion necesaria para el server del archivo de configuracion
+	// crea el socket, crea la cola de mensajes
 	int parkingManager(char *configFile);
 
+	// Abre el archivo de configuracion y parsea la informacion para escribir el resultado en port
 	int configServer(char *configFile, char *port);
 
+	// Se crea el contexto necesario para el funcionamiento de los hilos, se pone el socket a escuchar y finalmente ante
+	// cada nueva conexion al socket, se llama a la funcion que atiende a cada cliente.
 	int runServer(serverConf *conf);
 
+	// Funcion que ejecuta cada hilo por cada nueva conexion
 	int threadWork(threadData *data);
 
 #endif
