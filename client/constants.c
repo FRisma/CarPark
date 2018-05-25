73c8822e7bce4c7bddbba48d5e964ce8ea6a0ffb
Franco Risma <email>
2018-05-25 01:47:38 +0000

    Minor improvements

---
+++ client/constants.c
@@ -0,0 +1,12 @@
+const char *HTTP_PUT = "PUT %s HTTP/1.1\n"\
+						"Content-Type: %s";
+
+const char *HTTP_GET = "GET %s HTTP/1.1\n"\
+						"Content-Type: %s";
+
+/*
+const unsigned int MAX_IP_LENGTH = 16;
+const unsigned int MIN_IP_LENGTH = 7;
+const unsigned int MAX_PORT_LENGTH = 6;
+const unsigned int MIN_PORT_LENGTH = 2;
+*/
---
+++ client/constants.h
@@ -0,0 +1,12 @@
+extern const char *HTTP_PUT;
+extern const char *HTTP_GET;
+/*
+extern const unsigned int MAX_IP_LENGTH;
+extern const unsigned int MIN_IP_LENGTH;
+extern const unsigned int MAX_PORT_LENGTH;
+extern const unsigned int MIN_PORT_LENGTH;
+*/
+#define MAX_IP_LENGTH 16
+#define MIN_IP_LENGTH 7
+#define MAX_PORT_LENGTH 6
+#define MIN_PORT_LENGTH 2
