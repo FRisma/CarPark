d4925f084d0362fa3a2ef91ca898f21d49f3e3ea
Franco Risma <email>
2018-06-20 15:35:17 +0000

    Fixing typo

--- client/statusTicket.c
+++ client/statusTicket.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <unistd.h>
 
-const char *kStatuseURI = "slot/%s";
+const char *kStatusURI = "slot/%s";
 
 #define debug 0
 
@@ -14,7 +14,7 @@ int statusTicket(int socketDescriptor, char *slotId) {
 	char httpResponse[512] = {'\0'};
 	char resource[16] = {'\0'};
 
-	snprintf(resource, strlen(kStatusURI) + strnlen(slotId,SLOT_ID_LENGTH) + 1, kDeleteURI, slotId);
+	snprintf(resource, strlen(kStatusURI) + strnlen(slotId,SLOT_ID_LENGTH) + 1, kStatusURI, slotId);
 	snprintf(httpRequest, strlen(HTTP_GET) + strlen(resource) + 1, HTTP_GET, resource); 
 
 	if (-1 == dispatch(socketDescriptor, httpRequest, httpResponse) ) {
